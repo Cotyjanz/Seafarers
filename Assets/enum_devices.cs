86c812fa0a36c54f0bd6c2e357e04e5696e98489
Schmoozer <Cotyjanz@users.noreply.github.com>
2019-02-22 13:51:34 -0600

    Initial Commit

---
+++ Assets/enum_devices.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enum_devices : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		for (int i = 0; i < (Input.GetJoystickNames()).Length; i++) {
+			Debug.Log(Input.GetJoystickNames()[i]);
+		}	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

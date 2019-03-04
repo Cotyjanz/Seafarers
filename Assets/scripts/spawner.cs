6b52d61e26bc3aeceeca7b70626d661a7ac4ecae
Schmoozer <Cotyjanz@users.noreply.github.com>
2019-03-03 21:29:19 -0600

    commit

---
+++ Assets/scripts/spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour {
+
+	public GameObject[] beets;
+	public Transform[] points;
+	public float beat = (60 / 130) * 2;
+	private float timer;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+		if (timer > beat) {
+			GameObject beet = Instantiate(beets[Random.Range(0, 2)], points[Random.Range(0, 4)]);
+			beet.transform.localPosition = Vector3.zero;
+			beet.transform.Rotate(transform.forward, 90 * Random.Range(0, 4));
+			timer -= beat;
+		}
+
+		timer += Time.deltaTime;
+		
+	}
+}

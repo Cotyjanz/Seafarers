d0c3ac3956a487b8072713ff1f2bc99b284bb10b
Schmoozer <Cotyjanz@users.noreply.github.com>
2019-03-10 13:56:15 -0600

    commit

---
+++ Assets/scripts/CaptainController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptainController : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+    	//Vector3 
+    	float dt = Time.deltaTime;
+    	//transform.Translate(1 * dt, 1 * dt, 1 * dt);
+
+    	transform.position = transform.position + Camera.main.transform.forward * 1 * Time.deltaTime;
+
+    	 //Vector2 joyState = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick); 
+  
+    }
+}
